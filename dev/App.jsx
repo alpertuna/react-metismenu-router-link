42c4350e3f49a1ae38b2edb0fbf59021670f53c9
H.Alper Tuna <halpertuna@gmail.com>
2016-11-19 15:40:33 +0300

    Updated dev application

--- dev/App.jsx
+++ dev/App.jsx
@@ -9,7 +9,7 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, Link } from 'react-router';
 import MetisMenu from 'react-metismenu';
 //import MetisMenu from '../../react-metismenu/src';
 import RouterLink from '../src/RouterLink';
@@ -18,35 +18,35 @@ const Menu1 = () => <div><u>Menu 1 View</u></div>;
 const Menu2 = () => <div><i>Menu 2 View</i></div>;
 const SubMenu = () => <div><s>SubMenu View</s></div>;
 
-  const menu = [
-    {
-      icon: 'dashboard',
-      label: 'Menu 1',
-      to: 'menu-1',
-    },
-    {
-      icon: 'bell',
-      label: 'Menu 2',
-      to: 'menu-2',
-    },
-    {
-      icon: 'bolt',
-      label: 'Menu 3',
-      content: [
-        {
-          icon: 'bolt',
-          label: 'Sub Menu',
-          to: 'sub-menu',
-        },
-      ],
-    },
-    {
-      icon: 'external-link',
-      label: 'External Link',
-      externalLink: true,
-      to: 'https://www.google.com',
-    },
-  ];
+const menu = [
+  {
+    icon: 'dashboard',
+    label: 'Menu 1',
+    to: 'menu-1',
+  },
+  {
+    icon: 'bell',
+    label: 'Menu 2',
+    to: 'menu-2',
+  },
+  {
+    icon: 'bolt',
+    label: 'Menu 3',
+    content: [
+      {
+        icon: 'bolt',
+        label: 'Sub Menu',
+        to: 'sub-menu',
+      },
+    ],
+  },
+  {
+    icon: 'external-link',
+    label: 'External Link',
+    externalLink: true,
+    to: 'https://www.google.com',
+  },
+];
 
 const App = props => {
   return (
@@ -56,6 +56,8 @@ const App = props => {
         LinkComponent={RouterLink}
       />
       <h2>Page Content</h2>
+      <Link to="menu-1">External Menu 1</Link>
+      <Link to="menu-2">External Menu 2</Link>
       {props.children || "Greeter Page"}
     </div>
   );
